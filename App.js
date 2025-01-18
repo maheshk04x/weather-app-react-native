b7087a678a0aa251da2d58a876e514f25d0cf376
MaheshK04 <maheshk04x@gmail.com>
2025-01-18 16:48:49 +0530

    Added drawer Navigation in Home

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+import AppNavigation from './navigation/AppNavigation';
 
 export default function App() {
   return (
-    <HomeScreen/>
+    <AppNavigation/>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
--- navigation/AppNavigation.js
+++ navigation/AppNavigation.js
@@ -1,9 +1,44 @@
+import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 import HomeScreen from "../screens/HomeScreen";
 import WeatherDetails from "../screens/WeatherDetails";
+import Favourites from "../screens/Favourites";
+import Recents from "../screens/Recents";
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const DrawerStack = () => {
+  return (
+    <Drawer.Navigator
+      initialRouteName="Home"
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#ffffb3",
+        },
+        headerShadowVisible: false,
+        drawerActiveTintColor: "black",
+        drawerContentStyle: {
+          backgroundColor: "#ffffb3",
+          },
+          drawerLabelStyle: {
+              fontSize: 17,
+              fontWeight: "bold",
+        }
+      }}
+    >
+      <Drawer.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerTitle: "" }}
+      />
+      <Drawer.Screen name="Favourites" component={Favourites} options={{headerTitle:"Locations", headerTitleAlign:"center"}}/>
+      <Drawer.Screen name="Recents" component={Recents} />
+    </Drawer.Navigator>
+  );
+};
 export default function AppNavigation() {
   return (
     <NavigationContainer>
@@ -16,8 +51,8 @@ export default function AppNavigation() {
         }}
       >
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
+          name="DrawerStack"
+          component={DrawerStack}
           options={{ headerShown: false }}
         />
         <Stack.Screen
